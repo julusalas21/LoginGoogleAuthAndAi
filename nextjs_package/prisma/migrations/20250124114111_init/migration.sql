2768c6931e3d9d39304578084497300bfda1f153
julusalas21 <julusalas21@gmail.com>
2025-01-24 05:00:29 -0800

    detects and asks you about your favorite movie

---
+++ nextjs_package/prisma/migrations/20250124114111_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "movie" DROP DEFAULT;
--- nextjs_package/src/app/login/[id]/page.tsx
+++ nextjs_package/src/app/login/[id]/page.tsx
@@ -1,5 +1,9 @@
+import { checkUser } from "@/actions/actions";
 import {prisma} from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default async function UserHome({params}:any){
     const user=await prisma.user.findUnique({
@@ -15,10 +19,44 @@ export default async function UserHome({params}:any){
         </main>
         )
     }
+    else if(user.movie==null){
+        return(
+            <div className="flex flex-col items-center pt-23 text-center">
+                <form action={async (e)=>{
+                    "use server";
+                    await prisma.user.update({
+                    where:{
+                        email: user.email
+                    },
+                    data: {
+                        movie: e.get('movieName') as string
+                    }
+                })
+                redirect(`/login/${user.id}`);
+            }
+                
+                } className="flex flex-col space-y-2">
+                    <input 
+                        type="text"
+                        name="movieName"
+                        placeholder="Favorite Movie?"
+                        className="text-black"
+                    />
+                    <button
+                        type="submit"
+                        className="bg-white text-black rounded-sm"
+                    >
+                    Ready!
+                    </button>
+                   </form>
+            </div>
+        );
+    }
     else{
         return (
         <main className="flex flex-col items-center gapy-y-5 pt-18 text-center">
             <h1>Hello, {user?.name}</h1>
+            <div>Your favorite movie is {user?.movie}</div>
             <Link href="/" className="underline">Click to Logout and Go Home</Link>
         </main>
     )}
